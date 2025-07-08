4c6fe1f1a50125f600f8da8ab2aa16663797585f
zhinbziyi <kangxinyou@gmail.com>
2025-07-08 09:12:43 +0800

    Create data.js

---
+++ HW1/data.js
@@ -0,0 +1,44 @@
+const animeData = {
+  data: {
+    title_chinese: "葬送的芙莉蓮",
+    title_japanese: "葬送のフリーレン",
+    authors: [
+      { name: "山田鐘人", type: "原作" },
+      { name: "阿部司", type: "作畫" }
+    ],
+    aired: {
+      manga: "2020/04/28 ~ 2024/12/25", // 最新140話為止（2024年12月）
+      anime: "2023/09/29 ~ 2024/03/22"
+    },
+    manga_publisher: "小學館",
+    manga_chapters: 140,
+    manga_volumes: 14,
+    anime_episodes: 28,
+    status: [{manga_status: "無限期休刊"},{anime_status:"動畫已完結"}],
+    anime_duration: "每集約 24 分鐘",
+    anime_staff: {
+      studio: "MADHOUSE",
+      director: "斋藤圭一郎",
+      series_composition: "鈴木智尋",
+      music: ["Evan Call"],
+      production: "日本電視台"
+    },
+    images: {
+      jpg: {
+        image_url: "https://cdn.myanimelist.net/images/anime/1015/138006.jpg"
+      }
+    },
+    trailers: [
+        {embed_url:"https://www.youtube.com/watch?v=lv5R6C3hz54&list=WL&index=19"},
+        {embed_url:"https://www.youtube.com/watch?v=E0d2uEQJbXs&list=WL&index=12"},
+        {embed_url:"https://www.youtube.com/watch?v=OIBODIPC_8Y&list=WL&index=1"}
+    ],
+    synopsis: "在歷經長達十年的冒險後，勇者一行人擊敗了魔王並走向分別。身為精靈魔法使的芙莉蓮，壽命遠遠超過人類，她回歸了四處旅行收集魔法的日常，對這段夥伴情誼似乎並未留下太深情感，隨著時間流逝，當她親眼送走曾並肩作戰的夥伴後，才意識到過往的旅途對她的影響，也開始產生後悔。為了彌補過去的遺憾，芙莉蓮踏上理解人類、建立真實羈絆的新旅程──冒險的終章，也是全新故事的開端。",
+    genres: [
+      { name: "冒險" },
+      { name: "溫馨" },
+      { name: "奇幻" }
+    ]
+  }
+};
+
